19f25da409ad9440acd6a33b07842ce6e0624497
Josh Evenson <jevenson@outlook.com>
2018-02-13 20:17:28 -0600

    added key emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public emittedCode: number;
+
+  public ngOnInit(): void {
+    document.addEventListener('keydown', (event: KeyboardEvent) => this.onKeydown(event));
+  }
+
+  private onKeydown(event: KeyboardEvent): void {
+    this.emitEvent(event);
+    this.updateKey(event);
+  }
+
+  private emitEvent(event: KeyboardEvent): void {
+    if (window.parent) {
+      window.parent.postMessage({ eventType: 'keydown', keyCode: event.keyCode }, '*');
+    }
+  }
+
+  private updateKey(event: KeyboardEvent): void {
+    this.emittedCode = event.keyCode;
+  }
 }
