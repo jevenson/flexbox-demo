6efcd42d6544b82edbba22b86809155ba01bbea3
Josh Evenson <jevenson@outlook.com>
2018-02-20 21:22:02 -0600

    flex grow

--- src/app/flex-grow/flex-grow.component.ts
+++ src/app/flex-grow/flex-grow.component.ts
@@ -13,16 +13,57 @@ import { FlexContainerComponent } from './../shared/flex-container/flex-containe
     ::ng-deep .child {
       flex-grow: 0;
     }
+
+    .math-container {
+      display: flex;
+      font-size: 30px;
+      justify-content: space-around;
+    }
+
+    .child {
+      border: 2px solid black;
+      padding: 2.5%;
+    }
   `]
 })
 export class FlexGrowComponent {
 
+  public readonly elementCount: number = 4;
+
+  public allFlexGrowValue: number = 0;
+  public individualFlexGrowValue: number = 0;
+
+  public get allWidth(): number {
+    return this.totalWidth === 0 ? 0 : this.allFlexGrowValue / this.totalWidth;
+  }
+
+  public get individualWidth(): number {
+    return this.totalWidth === 0 ? 0 : this.individualFlexGrowValue / this.totalWidth;
+  }
+
+  private totalWidth: number = 0;
+
   @ViewChild(FlexContainerComponent) public flexContainer: FlexContainerComponent;
 
   public onPropertyValuechange(event): void {
-    const childSelector: string = event.propertyValue === '0' || event.propertyValue === '1' ? '.child' : undefined;
+    let childSelector: string | undefined = undefined;
 
-    console.log(childSelector);
+    let requestFlexValue: number = Number.parseInt(event.propertyValue);
+
+    if (requestFlexValue === 0 || requestFlexValue === 1) {
+      childSelector = '.child';
+
+      this.allFlexGrowValue = requestFlexValue;
+
+      this.totalWidth = this.allFlexGrowValue * this.elementCount;
+    }
+    else {
+      this.totalWidth = this.allFlexGrowValue * (this.elementCount - 1) + requestFlexValue;
+    }
+
+    this.individualFlexGrowValue = requestFlexValue;
+
+    console.log('total ' + this.totalWidth);
 
     this.flexContainer.applyStyle(event.propertyName, event.propertyValue, true, childSelector);
   }
